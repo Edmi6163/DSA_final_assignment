174d48e4dfb9afb41422772b3861dfe33b7d744a
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-10-28 19:21:05 +0200

    small changes

--- ex3_4/Prim.java
+++ ex3_4/Prim.java
@@ -1,15 +1,16 @@
-import ex3_4.exceptions.ElementNotFoundException;
-import ex3_4.exceptions.GraphException;
-import ex3_4.structures.Graph;
-import ex3_4.structures.PriorityQueue;
 
-import java.util.HashMap;
+import exceptions.ElementNotFoundException;
+import exceptions.GraphException;
+import structures.Graph;
+import structures.PriorityQueue;
+
+import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Prim {
-    public static ex3_4.structures.Graph<String,Float> graph;
+    public static Graph<String,Float> graph;
     //main must do:
     //1. read the graph from the csv file args[1]
     //2. calculate the minimum spanning forest with prim's algorithm
@@ -35,7 +36,7 @@ public class Prim {
     try {
       File file = new File(path);
       Scanner scanner = new Scanner(file);
-      ex3_4.structures.Graph<String,Float> builder = new Graph<>(true);
+      Graph<String,Float> builder = new Graph<>(true);
       while(scanner.hasNextLine()) {
         String line = scanner.nextLine();
         String[] values = line.split(",");
@@ -58,13 +59,10 @@ public class Prim {
    * @brief: generic implementation of a Prim's minimum spanning tree algorithm
    */
   public static Graph<String,Float> mst(Graph <String,Float> graph){
+		System.out.println("calculating mst: ");
     Graph<String,Float> mst = new Graph<>(true);
     Set<String> visited = new HashSet<>();
-    PriorityQueue<String,Float> edgeQueue = new PriorityQueue<>((v1,v2) -> {
-      Float weight1 = graph.map.get(v1).get(v2);
-      Float weight2 = graph.map.get(v2).get(v1);
-      return Float.compare(weight1, weight2);
-    });
+    PriorityQueue<String,Float> edgeQueue = new PriorityQueue<>(Comparator.comparing(v1 -> graph.map.get(v1),Float::compare));
     if(graph.map.isEmpty()) {
       return mst;
     }
@@ -96,4 +94,6 @@ public class Prim {
     }
     return mst;
   }
+
+
 }
