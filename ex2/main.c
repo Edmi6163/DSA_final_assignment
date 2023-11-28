f4ee386f7af6037fdc29d797cccfd341fe117c27
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-11-28 16:50:23 +0100

    tokenizing phrase now works

--- ex2/main.c
+++ ex2/main.c
@@ -1,128 +1,68 @@
 #include "skip_list.h"
+#include <stdio.h>
+#include <string.h>
 
 #define MAXW 64 // usually a words isn't longer than 64 characters
 #define MAXC 256
 
-int get_phrase(const char *file_name, char *arr[64])
-{
-	FILE *fp = fopen(file_name, "r");
-	if (fp == NULL)
-	{
-		printf("Error opening file\n");
-		exit(EXIT_FAILURE);
-	}
 
-	printf("Loading array from file \033[1m%s\033[22m \0337\033[5m...\n", file_name);
+void reading_dictionary(const char *dictfile, struct _SkipList *list) {
 
-	bool cont = false;
-	char c;
-	int words = 0, chars = 0;
+  char buffer[MAXC];
+  FILE *fp = fopen(dictfile, "r");
 
-	arr[words] = calloc(MAXW, sizeof(char)); // first word needs to be manually alloc'd
-	if (arr[words] == NULL && MAXW > 0)
-	{
-		printf("Error allocating memory\n");
-		exit(EXIT_FAILURE);
-	}
-	while (((c = fgetc(fp)) != EOF) && words < MAXW)
-	{
-		if (c < 'A' || (c > 'Z' && c < 'a') || c > 'z')
-		{
-			if (!cont)
-				continue; // makes fgetc loop until it finds a letter
+  printf("Loading dictionary from file \x1b[33m%s\n\x1b[0m", dictfile);
+  TEST_ERROR
 
-			arr[words][chars] = '\0';
-			chars = 0;
-			arr[++words] = calloc(MAXW, sizeof(char));
-			if (arr[words] == NULL && MAXW > 0)
-			{
-				printf("Error allocating memory\n");
-				exit(EXIT_FAILURE);
+  while (fgets(buffer, sizeof(buffer), fp)) {
+    strtok(buffer, "\n");
+    char *word_to_insert = malloc(strlen(buffer) + 1);
+    strcpy(word_to_insert, buffer);
+    if (!word_to_insert) {
+      TEST_ERROR
+      exit(1);
+    }
+    insert_skiplist(list, word_to_insert);
+  }
+	printf("dictionary loaded\n");
+
+ 
+  fclose(fp);
+}
+
+void find_errors(const char *dictfile, const char *textfile) {
+  struct _SkipList *list;
+	FILE *phrase = fopen(textfile,"r");
+  new_skiplist(&list, 10, compare_string);
+  reading_dictionary(dictfile, list);
+
+	char line[1024];
+
+  printf("looking for any errors .... \n");
+	while(fgets(line,sizeof(line),phrase)){
+		char *token = strtok(line," \t\n\r.,;:!-");
+		while(token){
+			if(!search_skip_list(list,token)){
+      printf("The word \033[31m%s\033[0m was not found, so probably is written incorrectly\n",token);
 			}
-			cont = false;
+			token = strtok(NULL, " \t\n\r.,;:!-");
 		}
-		else
-		{
-			if (c >= 'A' && c <= 'Z')
-				c += 32; // lowercase
-			arr[words][chars++] = c;
-			cont = true;
-		}
-	}
-	printf("\033[25m\0338\033[32mdone\033[0m\n");
-
-	if (!cont)
-		free(arr[words--]); // fixes case in which while loop thinks there's a word to be read but there's only random chars
-
-	fclose(fp);
-	return words;
-}
-
-void reading_dictionary(const char *dictfile, struct _SkipList *list)
-{
-
-	char buffer[128];
-	int words_count = 0;
-	FILE *fp = fopen(dictfile, "r");
-
-	printf("Loading dictionary from file \x1b[33m%s\n\x1b[0m", dictfile);
-	TEST_ERROR
-
-	for (int i = 0; fgets(buffer, sizeof(buffer), fp) != NULL; i++)
-	{
-		char *word = calloc(MAXW, sizeof(char));
-		if (word == NULL && MAXW > 0)
-		{
-			printf("Error allocating memory \n");
-			exit(EXIT_FAILURE);
-		}
-		sscanf(buffer, "%s", word);
-		printf("%s word %s \n",__FILE__, word);
-		insert_skiplist(list, word);
-		printf("%s inserted word %s \n",__FILE__, word);
-		words_count++;
-		printf("words count is %d\n",words_count);
-		//FIXME after this print the program give Fatal glibc error: malloc.c:2594 (sysmalloc): assertion failed: (old_top == initial_top (av) && old_size == 0) || ((unsigned long) (old_size) >= MINSIZE && prev_inuse (old_top) && ((unsigned long) old_end & (pagesize - 1)) == 0)
-// make: *** [makefile:14: run] Aborted (core dumped)
-		free(word);
-	}
-	printf("\n completed dictionary with %d\n",words_count);
-	fclose(fp);
-}
-
-void find_errors(const char *dictfile, const char *textfile, size_t max_height)
-{
-	struct _SkipList *list = NULL;
-	new_skiplist(&list, 10, compare_string);
-	printf("reading dictionary from file  %s\n", dictfile);
-	reading_dictionary(dictfile, list);
-	char *phrase[MAXW] = {0};
-	int words = get_phrase(textfile, phrase);
-	printf("words amount is %d\n", words);
-
-	for (int i = 0; i < words; i++)
-	{
-		printf("checking word %s\n", phrase[i]);
-		if (search_skip_list(list, phrase[i]) == NULL)
-			printf("%s\n", phrase[i]);
+		fclose(phrase);
+		clear_skiplist(&list);
 	}
 
-	clear_skiplist(&list);
-	printf("\n");
 }
 
-int main(int argc, char **argv)
-{
-	const char *dict = argv[1];
-	const char *corr = argv[2];
+int main(int argc, char **argv) {
+  const char *dict = argv[1];
+  const char *corr = argv[2];
 
-	printf(":::>dict path %s<::: \n", dict);
-	printf(":::>corr path %s<::: \n", corr);
+  printf(":::>dict path %s<::: \n", dict);
+  printf(":::>corr path %s<::: \n", corr);
 
-	if (argc < 3)
-		printf("not enough argument passed \n");
-	TEST_ERROR
-	printf("looking for any errors .... \n");
-	find_errors(dict, corr, 10);
-	return 0;
+  if (argc < 3)
+    printf("not enough argument passed \n");
+  TEST_ERROR
+  find_errors(dict, corr);
+  return 0;
 }
