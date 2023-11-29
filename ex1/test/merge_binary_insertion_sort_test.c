f704953ae8880fe18e94e44603131d72bcba861c
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-11-29 20:54:44 +0100

    compare functions for tests done

--- ex1/test/merge_binary_insertion_sort_test.c
+++ ex1/test/merge_binary_insertion_sort_test.c
@@ -2,11 +2,12 @@
 #include "unity.h"
 #include "unity_internals.h"
 #include <assert.h>
+#include "generic_comparable.h"
 
 void test_int_array_with_k_six(void) {
   static int actual[] = {8, 9, 78, 653, 5};
   static int expected[] = {5, 8, 9, 78, 653};
-  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 6, compare_int);
+  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 6, compare_int_test);
   TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 6);
 }
 
@@ -19,21 +20,21 @@ void test_string_array_with_key_six(void) {
   static char f[] = "ba";
   static char *actual[] = {a, b, c, d, e, f};
   static char *expected[] = {a, e, f, d, c, b};
-  merge_binary_insertion_sort(actual, 6, sizeof(char *), 6, compare_string);
+  merge_binary_insertion_sort(actual, 6, sizeof(char *), 6, compare_string_test);
   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 5);
 }
 
 void test_double_array_with_k_six(void) {
-  static double actual[] = {8.0, 9.0, 78.0, 653.0, 5.0};
-  static double expected[] = {5.0, 8.0, 9.0, 78.0, 653.0};
-  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 6, compare_double);
-  TEST_ASSERT_EQUAL_DOUBLE_ARRAY(expected, actual, 6);
+  static double actual[5] = {8.0, 9.0, 78.0, 653.0, 5.0};
+  static double expected[5] = {5.0, 8.0, 9.0, 78.0, 653.0};
+  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 6, compare_double_test);
+  TEST_ASSERT_EQUAL_DOUBLE_ARRAY(expected, actual, 5);
 }
 
 void test_int_array_with_k_one(void) {
   static int actual[] = {8, 9, 78, 653, 5};
   static int expected[] = {5, 8, 9, 78, 653};
-  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 1, compare_int);
+  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 1, compare_int_test);
   TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 6);
 }
 
@@ -46,14 +47,14 @@ void test_string_array_with_key_one(void) {
   static char f[] = "ba";
   static char *actual[] = {a, b, c, d, e, f};
   static char *expected[] = {a, e, f, d, c, b};
-  merge_binary_insertion_sort(actual, 6, sizeof(actual[0]), 1, compare_string);
+  merge_binary_insertion_sort(actual, 6, sizeof(actual[0]), 1, compare_string_test);
   TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 5);
 }
 
 void test_double_array_with_k_one(void) {
   static double actual[] = {8.0, 9.0, 78.0, 653.0, 5.0};
   static double expected[] = {5.0, 8.0, 9.0, 78.0, 653.0};
-  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 1, compare_double);
+  merge_binary_insertion_sort(actual, 5, sizeof(actual[0]), 1, compare_double_test);
   TEST_ASSERT_EQUAL_DOUBLE_ARRAY(expected, actual, 6);
 }
 
