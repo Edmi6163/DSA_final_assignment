af87c6acc25689114682e25d66ba49199904acac
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-12-07 15:35:07 +0100

    updated graph, setted main to begin time testing

--- ex1/main.c
+++ ex1/main.c
@@ -8,7 +8,6 @@ void sort_records(const char *infile, const char *outfile, size_t k, size_t fiel
   clock_t start, end;
   FILE *file_in = fopen(infile, "r");
   struct Records *array = create_array(file_in, &nitems);
-  fclose(file_in);
   printf("array created without errors\n");
    switch (field)
   {
@@ -31,7 +30,8 @@ void sort_records(const char *infile, const char *outfile, size_t k, size_t fiel
     printf("Invalid field\n");
     exit(EXIT_FAILURE);
   }
-  printf("Array sorted in \x1b[35m%lf\x1b[0m  seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+  // printf("Array sorted in \x1b[35m%lf\x1b[0m  seconds with k: \x1b[35m%zu\x1b[0m\n", (double)(end-start)/CLOCKS_PER_SEC,k);
+  printf("%lf,%zu", (double)(end-start)/CLOCKS_PER_SEC,k);
 
   FILE *f_out = fopen(outfile, "w");
   if(f_out == NULL){
@@ -42,6 +42,7 @@ void sort_records(const char *infile, const char *outfile, size_t k, size_t fiel
   for(int i=0;i<nitems;i++){
     fprintf(f_out,"%d,%s,%d,%lf\n",array[i].eId,array[i].field1,array[i].field2,array[i].field3);
   }
+  fclose(file_in);
   fclose(f_out);
   free(array); 
 }
--- ex1/time_taker_ex1.sh
+++ ex1/time_taker_ex1.sh
@@ -5,23 +5,29 @@ ALGORITHM_EXECUTABLE="./build/main/ex1 ../data/records.csv ../data/sorted.csv"
 
 # Specify the range of max heights you want to test
 MIN_K_VALUE=1
-MAX_K_VALUE=100
+MAX_K_VALUE=750
+INCREMENT=250
+FIELD=1
 
 # Counter for segmentation faults
 SEG_FAULT_COUNT=0
 
 # Loop through max heights
-for ((height = $MIN_HEIGHT; height <= $MAX_HEIGHT; height++)); do
+for ((k = $MIN_K_VALUE; k <= $MAX_K_VALUE; k++)); do
     # Run your algorithm with the current max height and measure the time
-    $ALGORITHM_EXECUTABLE $height
+    $ALGORITHM_EXECUTABLE $k $FIELD
 
     # Check the exit status of the last command
     if [ $? -ne 0 ]; then
-        echo "Segmentation fault detected for max height: $height"
+        echo "Segmentation fault detected for max height: $k"
         ((SEG_FAULT_COUNT++))
+    fi
+
+    # Increase the FIELD value every 250 runs
+    if [ $((k % INCREMENT)) -eq 0 ]; then
+        ((FIELD++))
     fi
 done
 
 echo "Script completed"
 echo "Number of segmentation faults: $SEG_FAULT_COUNT"
-
